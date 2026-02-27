4d07c0e36fb2bf99b600a033b418430d84eb0f67
bryan-ctrl <bryaninformation10@gmail.com>
2026-02-27 16:21:44 +0800

    New loading screen

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,11 @@
 export default function Loader() {
   return (
     <div className="loader-screen">
-      <div className="loader-spinner"></div>
-      <p className="loader-text">"Hello World.."</p>
+      <div className="logo-wrapper">
+        <i className="devicon-react-original colored loader-logo"></i>
+        <div className="glow-ring"></div>
+      </div>
+      <p className="loader-text">Loading Bryan's Portfolio</p>
     </div>
   )
 }
